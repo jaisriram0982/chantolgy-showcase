c7125085b5ce4dd7adebbc5cab90240139aa319b
Piyush Eklavya <70015666+piyusheklavya777@users.noreply.github.com>
2025-06-27 06:35:45 +0530

    feat: implement landing page with hero, games, and info sections

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,17 +1,25 @@
 import { NavBar } from "@/components/Navbar/NavBar";
+import { SectionHero } from "@/components/SectionHero/SectionHero";
+import { SectionWhatIsChantolgy } from "@/components/SectionWhatIsChantolgy/SectionWhatIsChantolgy";
+import { SectionWhyChantingGames } from "@/components/SectionWhyChantingGames/SectionWhyChantingGames";
+import { SectionOurGames } from "@/components/SectionOurGames/SectionOurGames";
+import { SectionComingSoon } from "@/components/SectionComingSoon/SectionComingSoon";
+import { SectionFooter } from "@/components/SectionFooter/SectionFooter";
 
-export default function Home() {
+export default function DraftPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-gray-700 via-gray-600 to-gray-400">
-      <NavBar className="fixed top-0 left-0 right-0 z-50" />
-      <div className="text-center mt-20">
-        <h1 className="text-4xl md:text-6xl font-bold text-gray-800 tracking-wide">
-          Chantolgy Studios
-        </h1>
-
-        <p className="mt-2 text-lg text-gray-400">
-          Under Maintenance. Stay tuned
-        </p>
+    <div className="flex flex-col items-center justify-start h-screen bg-black">
+      <NavBar className="fixed top-0 left-0 right-0 z-50 w-full" />
+      <div
+        id="sections-container"
+        className="mt-21 w-full lg:max-w-[80%] mx-auto flex flex-col"
+      >
+        <SectionHero />
+        <SectionWhatIsChantolgy />
+        <SectionWhyChantingGames />
+        <SectionOurGames />
+        <SectionComingSoon />
+        <SectionFooter />
       </div>
     </div>
   );
